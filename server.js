52a4ada6c1e0818880a4c72d98ee161895a03581
Lmickk2 <lukemickan21@icloud.com>
2022-12-11 17:52:48 -0500

    added to server.js

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const routeHTML = require("./routes/routeHTML")
 
 const express = require("express")
 const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}))
+app.use("/", routeHTML)
+app.use("/api", routeAPI)
+
+app.listen(PORT, () => {
+    console.log(`Navigate to http://localhost:${PORT}`)
+})
